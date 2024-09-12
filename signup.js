191be725db8e4cfe61d0a1c57b8e213971005c7b
manticike <antwikisaac@gmail.com>
2024-09-12 00:20:35 +0000

    I'm late to commit

--- signup.js
+++ signup.js
@@ -1,12 +1,14 @@
-const input = document.querySelector("#phone");
+const phoneInput = document.querySelectorAll(".phone");
 
-window.intlTelInput(input, {
-  initialCountry: "auto",
-  geoInLookup: function(callback) {
-    fetch('https://ipinfo.io?token=d4823601c9260b')
-      .then(response => response.json())
-      .then(data => callback(data.country))
-      .catch(() => callback("gh"));
-  },
-   utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-})
+phoneInput.forEach(input => {
+  window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoInLookup: function(callback) {
+      fetch('https://ipinfo.io?token=d4823601c9260b')
+        .then(response => response.json())
+        .then(data => callback(data.country))
+        .catch(() => callback("gh"));
+    },
+     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
+  })
+});
