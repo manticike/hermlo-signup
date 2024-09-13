dc64b6cd64f4c8c83537b53b25ab0f6616f972f8
manticike <antwikisaac@gmail.com>
2024-09-13 00:58:40 +0000

    Working on the top navigation
    Making the top menu responsive using media
    queries

--- signup.js
+++ signup.js
@@ -1,10 +1,15 @@
 const phoneInput = document.querySelectorAll(".phone");
+const hamburgerMenu = document.querySelector('#hamburger');
+const closeBtn = document.querySelector('.close-btn');
+const navMenu = document.querySelector('.nav-menu');
+
 document.querySelector('#current-year').textContent = new Date().getFullYear();
+
 
 phoneInput.forEach(input => {
   window.intlTelInput(input, {
     initialCountry: "auto",
-    geoInLookup: function(callback) {
+    geoIpLookup: function(callback) {
       fetch('https://ipinfo.io?token=d4823601c9260b')
         .then(response => response.json())
         .then(data => callback(data.country))
@@ -12,4 +17,16 @@ phoneInput.forEach(input => {
     },
      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
   })
+});
+
+
+hamburgerMenu.addEventListener('click', function() {
+  document.querySelector('#navMenu').classList.toggle('active');
+  closeBtn.style.display = 'block'; // Shows the close button
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  closeBtn.style.display = none; // Hide close button
+  hamburgerMenu.style.display = 'block'; //
 });
